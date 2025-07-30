eb39d0c04b970533884ca848dc30018bd20ad0c0
Esteban Forero <estebanmff@outlook.com>
2025-07-30 08:50:22 -0500

    Initial commi

---
+++ src/main/java/com/brute_force/brute_force/BruteForceApplication.java
@@ -0,0 +1,13 @@
+package com.brute_force.brute_force;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruteForceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BruteForceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brute_force/brute_force/BruteForceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brute_force.brute_force;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruteForceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
